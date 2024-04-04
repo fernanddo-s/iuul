87b32f95b194e30297194418db61c9a0f564e2d0
Luis Fernando <fernando.osousa@alu.ufc.br>
2024-04-03 21:52:30 -0300

    add: classe de validação

--- Desafio2.1/src/ReadFile.js
+++ Desafio2.1/src/ReadFile.js
@@ -2,20 +2,13 @@ let fs = require("fs");
 
 class ReadFile {
   readFileWithPath(filePath) {
-    fs.readFile(`${filePath}`, 'utf-8', (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      const user = JSON.parse(data)
-      console.log(user.nome)
-      return user;
-    });
+    try{
+      const users = fs.readFileSync(`${filePath}`, "utf-8");
+      return JSON.parse(users);
+    }catch(e){
+      console.log(e)
+    }
   }
 }
-  
+
 module.exports = ReadFile;
-
-// const fs = require('fs');
-
-// const dados = fs.readFileSync('arquivo.json', 'utf8');
-// console.log(JSON.parse(dados));
--- Desafio2.1/src/validacao.js
+++ Desafio2.1/src/validacao.js
@@ -1,11 +1,14 @@
 const { argv } = require('node:process');
 const ReadFile = require('./ReadFile');
 const WriteFile = require('./WriteFile');
+const Validation = require('./Validation');
 
 let rdfl = new ReadFile();
-const data = rdfl.readFileWithPath(argv[2]);//melhorar o nome da variável
-console.log(argv[2])
-console.log(typeof data)
+const data = rdfl.readFileWithPath(argv[2]);
+
+const validation = new Validation();
+
+validation.JSONValidation(data);
 
 
 let wrfl = new WriteFile();
