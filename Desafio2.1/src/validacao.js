d90eb4463accfe36f60ed4ff700efcf0f5177cc6
Luis Fernando <fernando.osousa@alu.ufc.br>
2024-04-03 22:42:41 -0300

    fix: indentacao

--- Desafio2.1/src/validacao.js
+++ Desafio2.1/src/validacao.js
@@ -10,15 +10,5 @@ const validation = new Validation();
 
 validation.JSONValidation(data);
 
-
 let wrfl = new WriteFile();
 wrfl.writeFileWithErros("teste")
-
-//executa o arquivo validação ok
-//ele chama o leitor ok
-//pega os dados do arquivo ok
-//chama a classe de Validação
-//executa a validação
-//chama a classe de escrita ok
-//escreve o que precisa ser escrito
-//fim do programa
