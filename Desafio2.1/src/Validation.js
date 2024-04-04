3d3fd5a79db7fd217d83c78e445270b950cd3ab1
Luis Fernando <fernando.osousa@alu.ufc.br>
2024-04-03 21:50:33 -0300

    Add: funções de validação

--- Desafio2.1/src/Validation.js
+++ Desafio2.1/src/Validation.js
@@ -1,10 +1,121 @@
-//recebe os dados e faz a validação e retorna alguma coisa
-
+const moment = require("moment");
 class Validation {
-  JSONValidation(json) {
-    //"quebrar" o json para validar cada dado
-    //talvez já receber um array de objetos ou ter uma função só pra isso
+  JSONValidation(users) {
+    for (let i = 0; i < users.length; i++) {
+      console.log(users[i].nome);
+      console.log(validarNome(users[i].nome));
+      // validarNome(users[i].nome);
+
+      console.log(users[i].cpf);
+      console.log(validarCPF(users[i].cpf));
+      // validarCPF(users[i].cpf);
+
+      //validarDataNascimento();
+      // validarRendaMensal();
+
+      console.log(users[i].estado_civil);
+      console.log(validarEstadoCivil(users[i].estado_civil));
+      // validarEstadoCivil(users[i].estado_civil);
+
+      // let date = new Date("2025-04-21")
+      // console.log(date.getFullYear())
+      // console.log(formatoValido(users[i].dt_nascimento));
+      // let data = validarData(users[i].dt_nascimento);
+      // console.log(data)
+      // console.log(formatoValido(data));
+      // idadeValida(calculaIdade(data));
+    }
   }
 }
 
 module.exports = Validation;
+
+// validar nome
+function validarNome(nome) {
+  if (nome.length < 5 || nome.length > 60) return false;
+
+  return true;
+}
+
+// validar cpf
+function validarCPF(cpf) {
+  if (cpf.length !== 11 || /^(.)\1+$/.test(cpf)) return false;
+  let sum = 0;
+  for (let i = 0; i < 9; i++) {
+    sum += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+  let remainder = 11 - (sum % 11);
+  let digit = remainder > 9 ? 0 : remainder;
+
+  if (parseInt(cpf.charAt(9)) !== digit) return false;
+
+  sum = 0;
+  for (let i = 0; i < 10; i++) {
+    sum += parseInt(cpf.charAt(i)) * (11 - i);
+  }
+  remainder = 11 - (sum % 11);
+  digit = remainder > 9 ? 0 : remainder;
+
+  if (parseInt(cpf.charAt(10)) !== digit) return false;
+
+  return true;
+}
+
+// validar data de nascimento
+function validarDataNascimento(data) {
+  //recebe uma string do tipo "dt_nascimento": "14091966"
+  //validar formato
+  //validar idade (18 anos ou mais)
+
+}
+
+function formatoValido(data) {
+  return /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}/.test(data);
+}
+
+function idadeValida(dataStr) {
+  let [dia, mes, ano] = dataStr.split("/");
+  let data = new Date(ano, mes, dia);
+  return calculaIdade(data) >= 18;
+}
+
+function calculaIdade(data) {
+  const hoje = new Date();
+  let idade = hoje.getFullYear() - data.getFullYear();
+  if (
+    hoje.getMonth() < data.getMonth() ||
+    (hoje.getMonth() === data.getMonth() && hoje.getDate() < data.getDate())
+  ) {
+    return idade - 1;
+  }
+  return idade;
+}
+
+function validarData(dt_nascimento) {
+  if (dt_nascimento.length !== 8) {
+    return null;
+  }
+  const data = moment(dt_nascimento, "YYYYMMDD");
+
+  if (data.isValid()) {
+    return data.format("DD/MM/YYYY");
+  } else {
+    return null;
+  }
+}
+// validar renda mensal
+function validarRendaMensal(renda) {}
+
+// validar estado civil
+function validarEstadoCivil(estadoCivil) {
+  estadoCivil = estadoCivil.toUpperCase();
+  if (
+    estadoCivil === "C" ||
+    estadoCivil === "S" ||
+    estadoCivil === "V" ||
+    estadoCivil === "D"
+  )
+    return true;
+
+  return false;
+}
