46a0192ce8a451f456e7a126a37faa0614c71de7
Luis Fernando <fernando.osousa@alu.ufc.br>
2024-04-24 08:55:24 -0300

    add: entrada de dados via console

--- Desafio3/src/index.js
+++ Desafio3/src/index.js
@@ -1,15 +1,19 @@
+const rl = require("readline-sync");
 (async () => {
+
+    const teste = rl.question('Teste: ');
+    console.log(teste)
 
     const db = require('./db/db');
     const Paciente = require('./models/Paciente');
     const Consulta = require('./models/Consulta');
     await db.sync();
 
-    // const novoPaciente = await Paciente.create({
-    //     cpf: "08228186398",
-    //     nome: "Luis Fernando",
-    //     dataNascimento: "2001-04-25"
-    // })
+    const novoPaciente = await Paciente.create({
+        cpf: "08228186399",
+        nome: teste,
+        dataNascimento: "2001-04-25"
+    })
 
     // const novaConsulta = await Consulta.create({
     //     data: "2024-04-27",
@@ -18,7 +22,7 @@
     //     idPaciente: novoPaciente.id
     // })
 
-    const paciente = await Paciente.findByPk(1, { include: Consulta });
+    // const paciente = await Paciente.findByPk(1, { include: Consulta });
     // const consultas = await paciente.getConsultas();
-    console.log(paciente.consulta);
+    // console.log(paciente.consulta);
 })();
