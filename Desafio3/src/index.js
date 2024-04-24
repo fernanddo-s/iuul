e54d5ebd3dc6ffdfe8be1fda724b21009c90fc09
Luis Fernando <fernando.osousa@alu.ufc.br>
2024-04-23 22:29:58 -0300

    add: relcionamento entre modelos

--- Desafio3/src/index.js
+++ Desafio3/src/index.js
@@ -2,11 +2,23 @@
 
     const db = require('./db/db');
     const Paciente = require('./models/Paciente');
+    const Consulta = require('./models/Consulta');
     await db.sync();
 
-    const novoPaciente = await Paciente.create({
-        cpf: "08228186398",
-        nome: "Luis Fernando",
-        dataNascimento: "2001-04-25"
-    })
+    // const novoPaciente = await Paciente.create({
+    //     cpf: "08228186398",
+    //     nome: "Luis Fernando",
+    //     dataNascimento: "2001-04-25"
+    // })
+
+    // const novaConsulta = await Consulta.create({
+    //     data: "2024-04-27",
+    //     horaInicial: "1000",
+    //     horaFinal: "1030",
+    //     idPaciente: novoPaciente.id
+    // })
+
+    const paciente = await Paciente.findByPk(1, { include: Consulta });
+    // const consultas = await paciente.getConsultas();
+    console.log(paciente.consulta);
 })();
